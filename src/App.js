f7c67b0cb8a15c1a280b94fe633bd401b8e4bf0a
Millborne-g <galamiton.millborne@gmail.com>
2022-11-23 18:38:02 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
@@ -6,28 +6,34 @@ import AddTask from "./components/AddTask";
 function App() {
   const [showAddForm, setshowAddForm] = useState(false)
   
-  const [tasks,setTask] = useState(
-    [
-      {
-        id: 1,
-        text: "task 1",
-        day: "day 1",
-        reminder: true
-      },
-      {
-        id: 2,
-        text: "task 2",
-        day: "day 2",
-        reminder: true
-      },
-      {
-        id: 3,
-        text: "task 3",
-        day: "day 3",
-        reminder: false
-      },
-    ]
-  )
+  const [tasks,setTask] = useState([])
+
+  useEffect(() =>{
+    const getData = async () =>{
+      const tasksFromServer = await fetchtData()
+      setTask(tasksFromServer)
+    } 
+    
+    getData()
+    
+  },[])
+
+  //fetch GET
+  const fetchtData = async () => {
+    const response = await fetch("http://localhost:5000/tasks")
+    const data = await response.json()
+    //console.log(data)
+
+    return data
+  }
+
+  /*
+  test
+  tasks.map((task) => {
+    console.log(task)
+  })
+  */
+  //console.log(fetchtData().then((db)=>console.log(db)))
 
   //Add task
   const addTask = (task) =>{
@@ -59,6 +65,7 @@ function App() {
         task
       ))
     )
+    
   }
 
   //Delete
@@ -73,14 +80,23 @@ function App() {
 
   return (
     <div className="container">
-      {/* Ang ginabuhat ani kay ga pasa syag "onAdd" na naay function daan sa "Button". Para if i click ang button ma triggerni sya na function*/}
+      {/* 
+        Ang "onAdd" kay ga contain syag execution (ma true ang value sa "showAddForm" sya once na e activate sa user) e activate ni sya didto sa Button.js
+        "addBtnTxt" to change color and text only
+      */}
       <Header title={"Time tracker"} onAdd={()=>setshowAddForm(!showAddForm)} addBtnTxt={showAddForm}/>
       {
         showAddForm &&
         <AddTask onAdd={addTask}/>
       }
+
+      {
+        tasks.length > 0?
+          <Tasks tasks={tasks} onDelete={onDelete} toggleReminder={toggleReminder}/> :
+          "no task"
+      }
       
-      <Tasks tasks={tasks} onDelete={onDelete} toggleReminder={toggleReminder}/>
+      
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,17 @@
 import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task,onDelete,toggleReminder}) => {
+  var clickCounter = 0
+  const doubleClick = (val) =>{
+    clickCounter=clickCounter+val
+    if (clickCounter > 1){
+      toggleReminder(task.id)
+      clickCounter = 0
+    }
+  }
   return (
     //if ang task.reminder kay true ma "task.reminder" iyang classname
-    <div className={`task ${task.reminder ? 'reminder':''}`} onDoubleClick={()=>toggleReminder(task.id)}>
+    <div className={`task ${task.reminder ? 'reminder':''}`} onClick={()=>doubleClick(1)}>
         <h3>{task.text}
         <FaTimes 
             style={{color: 'red', cursor: 'pointer'}}
